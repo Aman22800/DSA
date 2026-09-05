60badec3ee6e03d1443cbf47b9b9af2a91b09d7c
Aman <aman027716@gmail.com>
2026-09-05 16:39:34 +0530

    Time: 52 ms (5.02%) | Memory: 19.5 MB (9.81%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.py
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.py
@@ -1,12 +1,21 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
+        #compute suffix min
+        suffix_min=[0]*len(nums)
+        curr_min=float('inf')
+        for i in range(len(nums)-1,-1,-1):
+            #print(i)
+            curr_min=min(nums[i],curr_min)
+            suffix_min[i]=curr_min
+            print(suffix_min)
+
+
         currmax=float('-inf')
 
         for i in range(len(nums)):
             currmax=max(currmax,nums[i])
-            currmin=min(nums[i:])
 
-            score=currmax-currmin
+            score=currmax-suffix_min[i]
             if score<=k:
                 return i
         return -1
